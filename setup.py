83f4a87f8d7ceb52e7a58c3d2ea7f948e9d5ffbc
jacksonsamuelmoore <jacksonsamuelmoore@gmail.com>
2021-11-23 11:30:41 +0000

    Add basic info to PyPI setup

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+import pathlib
 from setuptools import setup
 
-setup()
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name="mrbenn",
+    version="0.0.1",
+    description="An addon for Django Debug Toolbar that allows for directly opening static files and views",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andytwoods/mrbenn",
+    author="Andy Woods",
+    author_email="",
+)
